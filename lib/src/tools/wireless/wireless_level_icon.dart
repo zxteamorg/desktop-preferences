b927eff98af1da61b3b1c422b91617b0ae1311b2
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-10-14 16:55:48 +0300

    Trying to add positional arguments into wirelessLevelIcon #2

--- lib/src/tools/wireless/wireless_level_icon.dart
+++ lib/src/tools/wireless/wireless_level_icon.dart
@@ -1,12 +1,22 @@
 import "package:flutter/material.dart";
 
-import "wireless_popup.dart" show WirelessPopup;
+// import "wireless_popup.dart" show WirelessPopup;
+// import "wireless_controller.dart" show WirelessController;
 
 class WirelessLevelIcon extends StatelessWidget {
-  final WirelessPopup wirelessLevelIconData;
-  const WirelessLevelIcon(this.wirelessLevelIconData, {Key? key, required IconData iconData, required Color color,})
+  final wirelessLevelIconData;
+  final isPublicIconData;
+  final iconData;
+  final color;
+
+  const WirelessLevelIcon(
+      {Key? key,
+      this.wirelessLevelIconData,
+      this.isPublicIconData,
+      this.iconData,
+      this.color})
       : super(key: key);
-   
+
   @override
   Widget build(BuildContext context) {
     return Container(

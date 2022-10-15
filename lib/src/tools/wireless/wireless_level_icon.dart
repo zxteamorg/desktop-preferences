e48eb4114d2e4c129c0ab6a346559c65774a6743
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-10-15 19:53:09 +0300

    Return positional arguments iconData, color

--- lib/src/tools/wireless/wireless_level_icon.dart
+++ lib/src/tools/wireless/wireless_level_icon.dart
@@ -1,26 +1,20 @@
 import "package:flutter/material.dart";
 
-// import "wireless_popup.dart" show WirelessPopup;
-// import "wireless_controller.dart" show WirelessController;
-
 class WirelessLevelIcon extends StatelessWidget {
-  // final wirelessLevelIconData;
-  final iconData;
-  final color;
+  final IconData iconData;
+  final Color? color;
 
-  const WirelessLevelIcon(
-      {Key? key,
-      // this.wirelessLevelIconData,
-      this.iconData,
-      this.color})
-      : super(key: key);
+  const WirelessLevelIcon({
+    Key? key,
+    required this.iconData,
+    this.color,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration:
-          const BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-      child: this.wirelessLevelIconData,
+    return Icon(
+      this.iconData,
+      color: this.color,
     );
   }
 }

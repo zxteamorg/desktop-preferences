24500c8d878f31b28b8882a77e1dc654803be27e
Dmitry Detukov <dmitry.detukov@zxteam.net>
2022-10-19 12:31:47 +0300

    Code not compiling StackOverflowError

--- lib/src/tools/wireless/wireless_level_icon.dart
+++ lib/src/tools/wireless/wireless_level_icon.dart
@@ -1,20 +1,35 @@
 import "package:flutter/material.dart";
 
+import "wireless_service_contract.dart" show WirelessLevel;
+
 class WirelessLevelIcon extends StatelessWidget {
-  final IconData iconData;
+  final WirelessLevel level;
   final Color? color;
 
   const WirelessLevelIcon({
     Key? key,
-    required this.iconData,
+    required this.level,
     this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Icon(
-      this.iconData,
+    return WirelessLevelIcon(
+      level: this.level,
       color: this.color,
     );
+  }
+
+    IconData resolveOtherNetworksIcon(WirelessLevel level) {
+    if (level == WirelessLevel.poor) {
+      return Icons.wifi_1_bar_rounded;
+    }
+    if (level == WirelessLevel.good) {
+      return Icons.wifi_1_bar;
+    }
+    if (level == WirelessLevel.nice) {
+      return Icons.wifi_2_bar;
+    }
+    return Icons.wifi;
   }
 }
